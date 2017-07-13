285d8609c27bff5f78c2a28a4e97a89082aebfc8
Trixie Devine <sward@Stacy-Wards-MacBook-Pro-3.local>
2017-07-13 10:08:09 -0400

    array set up

---
+++ minedmindsarray.rb
@@ -0,0 +1,7 @@
+numbers = 1
+onehundred = []
+100.times do
+	onehundred[numbers-1]=numbers
+	numbers = numbers + 1
+end
+p onehundred
--- minedmindsarray_test.rb
+++ minedmindsarray_test.rb
@@ -3,7 +3,8 @@ require_relative "minedmindsarray.rb"
 
 class TestEvenOdd < Minitest::Test
 
-
-
+def assert_that_1_equals_1
+	assert_equal(1, 1)
+end
 
 end
